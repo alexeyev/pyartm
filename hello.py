4f644a01b2aa7007df69d9f6a69374fa74b98efa
alexeyev <anton.m.alexeyev@gmail.com>
2014-10-22 03:49:12 +0400

    Костыли @ велосипеды

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@
 
 import nltk
 import textmining
+import numpy
 
 stemmer = nltk.stem.porter.PorterStemmer()
 tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
@@ -11,19 +12,23 @@ def get_words(text):
     tokenized = tokenizer.tokenize(text)
     return [stemmer.stem(t).lower() for t in tokenized]
 
-text1 = """Alice was published in 1865, three years after Charles Lutwidge Dodgson and the Reverend Robinson Duckworth rowed in a boat, on 4 July 1862[4] (this popular date of the "golden afternoon"[5] might be a confusion or even another Alice-tale, for that particular day was cool, cloudy and rainy[6]), up the Isis with the three young daughters of Henry Liddell (the Vice-Chancellor of Oxford University and Dean of Christ Church): Lorina Charlotte Liddell (aged 13, born 1849) ("Prima" in the book's prefatory verse); Alice Pleasance Liddell (aged 10, born 1852) ("Secunda" in the prefatory verse); Edith Mary Liddell (aged 8, born 1853) ("Tertia" in the prefatory verse)."""
+def norm(text):
+    return " ".join(get_words(text))
 
-text2 = """The Queen of Hearts She made some tarts,All on a summer's day;The Knave of Hearts He stole those tarts,And took them clean away.The King of Hearts Called for the tarts,And beat the knave full sore;The Knave of Hearts Brought back the tarts,And vowed he'd steal no more. """
 
-text3 = """The Queen of Hearts She made some tarts,All on a summer's day;The Knave of Hearts He stole those tarts,And took them clean away.The King of Hearts Called for the tarts,And beat the knave full sore;The Knave of Hearts Brought back the tarts,And vowed he'd steal no more. """
+def build_matrix(texts):
+    """
+    texts are filenames
+    inefficient implementation
+    """
+    tdm = textmining.TermDocumentMatrix()
+    for text in texts:
+        data = open(text, "r").read()
+        tdm.add_doc(norm(data))
+    return numpy.array([row for row in tdm.rows(cutoff=0)][1:])
 
-#print get_words(text1)
-#print get_words(text2)
 
-tdm = textmining.TermDocumentMatrix()
-tdm.add_doc(text1)
-tdm.add_doc(text2)
-tdm.add_doc(text3)
+def em(topics, tdm):
+    return 0
 
-for row in tdm.rows(cutoff=1):
-    print row
+print build_matrix(["cartoon.txt", "book.txt"])
