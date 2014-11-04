c60ec582d18ca06be82c74f5e8b696562d3eb08e
alexeyev <anton.m.alexeyev@gmail.com>
2014-11-04 19:17:15 +0400

    to lil_matrix

--- script.py
+++ script.py
@@ -1,16 +1,17 @@
 # !/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sklearn.feature_extraction as fe
 import nltk
+import numpy
 from scipy import *
 from scipy.sparse import *
+import sklearn.feature_extraction as fe
 
 
 def build_tdm(filenames):
     texts = [open(fn, "r+").read() for fn in filenames]
     stopwords = nltk.corpus.stopwords.words('english')
-    vectorizer = fe.text.CountVectorizer(min_df=2, ngram_range=(1, 2), max_df=0.5, stop_words=stopwords)
+    vectorizer = fe.text.CountVectorizer(min_df=0.2, ngram_range=(1, 2), max_df=1, stop_words=stopwords)
 
     # строки -- документы, столбцы -- слова
     X_words = vectorizer.fit_transform(texts)
@@ -19,7 +20,7 @@ def build_tdm(filenames):
 
 def init_matrices(terms, docs, topics):
     """ phi_w,t = p(w | t), theta_t,d = p(t | d) """
-    phi = csc_matrix((terms, topics))
+    phi = lil_matrix((terms, topics))
 
     for i in xrange(min(terms, topics)):
         phi[i, i] = 1.0
@@ -33,7 +34,7 @@ def init_matrices(terms, docs, topics):
 
 
 def sm(rows, columns):
-    return csc_matrix((rows, columns))
+    return lil_matrix((rows, columns))
 
 
 def em(tdm, topics, iterations):
@@ -45,9 +46,13 @@ def em(tdm, topics, iterations):
         print i, "iteration"
 
         nwt, ntd, nt, nd = sm(words, topics), sm(topics, docs), sm(topics, 1), sm(docs, 1)
+        print "matrices set"
 
         for d in xrange(docs):
+            print "doc", d
             for w in xrange(words):
+                if w % 500 == 0:
+                    print w,
                 phi_theta_dw = 0.0
 
                 for t in xrange(topics):
@@ -60,7 +65,10 @@ def em(tdm, topics, iterations):
                     ntd[t, d] += exp
                     nt[t, 0] += exp
                     nd[d, 0] += exp
+            print
+
         for t in xrange(topics):
+            print "topic", t
             for w in xrange(words):
                 phi[w, t] = nwt[w, t] / (nt[t, 0] + 0.0001)
             for d in xrange(docs):
@@ -74,11 +82,11 @@ onlyfiles = [join(path,f) for f in listdir(path) if isfile(join(path,f))]
 
 print onlyfiles
 
-tdm = build_tdm(onlyfiles)[1]
+tdm = build_tdm(onlyfiles[:4])[1]
 
 print tdm
 print tdm.shape
 
-wt, td = em(tdm, 2, 2)
+wt, td = em(tdm, 10, 2)
 
 print wt.todense()
